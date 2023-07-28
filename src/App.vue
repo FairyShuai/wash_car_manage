93ca27d71147a53c94208ff579360a396311852f
HuanGitHub <601335832@qq.com>
2023-07-28 21:11:31 +0800

    配置文件别名和环境变量

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,15 @@
 <template>
-  <div></div>
+  <div>
+    <el-pagination
+      :page-size="100"
+      layout="total, prev, pager, next"
+      :total="1000"
+    />
+    <el-button type="primary">Primary</el-button>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+</script>
 
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+// @ts-expect-error
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs';
+import App from '@/App.vue';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(ElementPlus, {
+  locale: zhCn,
+});
+console.log(import.meta.env);
+app.mount('#app');
